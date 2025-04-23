0a9f3957b8e63544df1d3545459edc0380d80d00
hafilpk <pkmshafil@gmail.com>
2025-04-23 18:29:07 +0530

    base.html created and extended

--- Home/views.py
+++ Home/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    numbers = {
+        'num': -10
+
+    }
+    return render(request, 'index.html', numbers)
 
 def about(request):
     return render(request, 'about.html')
