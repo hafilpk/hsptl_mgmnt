bd50d7c9b5a89a10cbc4523cab596785028d1d13
hafilpk <pkmshafil@gmail.com>
2025-04-25 18:36:22 +0530

    Static folder added and linked style.css

--- Home/views.py
+++ Home/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    numbers = {
-        'num': -10
-
-    }
-    return render(request, 'index.html', numbers)
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
